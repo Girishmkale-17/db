301adbc20c6561ee694fefa6b31372192d809f5c
girishmkale <girishmkale@gmail.com>
2019-12-02 23:28:11 +0530

    first commit

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table emp (id int, name varchar(50), address varchar(50));
+create table person (adhar int, name varchar(50), address varchar(50));
+
+insert into emp values(1, "Girish", "Jalgaon");
+insert into emp values(2, "Harshal", "Dhule");
+insert into emp values(3, "Abhi", "Pune");
